71a2950a84119343a0f0b975e7909504181a3a75
priscahlimo <priscahlimoh@gmail.com>
2022-02-20 20:54:38 +0300

    create key and text parameters and its constructor

--- src/main/java/order/Encrypt.java
+++ src/main/java/order/Encrypt.java
@@ -1,5 +1,11 @@
 package order;
 
 public class Encrypt {
+    private int mKey;
+    private String mText;
 
+    Encrypt(String text, int key) {
+        this.mText = text;
+        this.mKey = key;
+    }
 }
