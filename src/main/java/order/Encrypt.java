b01a56831ab387228fb540d68d23328fbd752f26
priscahlimo <priscahlimoh@gmail.com>
2022-02-20 22:47:29 +0300

    add a for loop that returns text encrypted

--- src/main/java/order/Encrypt.java
+++ src/main/java/order/Encrypt.java
@@ -33,5 +33,22 @@ public class Encrypt {
         else if (Text.isEmpty() && Key>= 0 && Key <= 25) {
             textEncrypted= "Please enter the text to be encrypted";
         }
-    };
+        else if ((Text.isEmpty() || !Text.isEmpty()) && (Key < 0 || Key > 25)) {
+            textEncrypted = "Key must be a number between 0 and 25";
+        }
+
+
+        char[] aCharacters = this.Text.toCharArray();
+
+        for (char character : aCharacters) {
+            if (!Text.isEmpty() && Key >= 1 && Key <= 25) {
+                int valueOfChar = Character.valueOf(character) + Key;
+                char shifted = (char) valueOfChar;
+
+                textEncrypted += Character.toString(shifted);
+            }
+        }
+        return textEncrypted;
+    }
+
 }
