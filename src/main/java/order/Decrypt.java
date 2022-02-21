c5777c554c18a34479414e9b659d67d69c44b053
priscahlimo <priscahlimoh@gmail.com>
2022-02-21 11:49:19 +0300

    initialize an if statement that checks if inputted texts meets the criteria

--- src/main/java/order/Decrypt.java
+++ src/main/java/order/Decrypt.java
@@ -20,13 +20,19 @@ public class Decrypt {
         this.key = key;
     }
 
-    public String Decrypt(int key, String reversed){
+    public String Decrypt(int key, String Text){
 
         this.key= key;
-        this.reversed=reversed;
+        this.reversed=Text;
 
         String textDecrypted="";
 
-
+        if (key == 0 && !Text.isEmpty()) {
+            textDecrypted = this.reversed;
+        } else if (Text.isEmpty() && key >= 0 && key <= 25) {
+            textDecrypted = "No text to encrypt,Enter text";
+        } else if ((Text.isEmpty() || !Text.isEmpty()) && (key < 0 || key > 25)) {
+          textDecrypted = "Key must be a number between 0 and 25";
+        }
     };
 }
