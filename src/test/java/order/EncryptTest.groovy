4440536f699a401cb83b453ae7bd72ce8cf6c5c0
priscahlimo <priscahlimoh@gmail.com>
2022-02-21 12:52:40 +0300

    write encryption test

--- src/test/java/order/EncryptTest.groovy
+++ src/test/java/order/EncryptTest.groovy
@@ -1,4 +1,29 @@
-package order
+package order;
 
-class EncryptTest {
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class EncryptTest {
+
+    @Test
+    public void encryptDecryptClassExists(){
+        Encrypt encryptDecrypt = new Encrypt ();
+        assertEquals(true,encryptDecrypt instanceof Encrypt);
+
+    }
+
+//    test if a character is encrypted
+
+    @Test
+    public void encryptsCharacter(){
+        Encrypt encryptDecrypt = new Encrypt();
+        assertEquals("b",encryptDecrypt.Encrypt(2,"d"));
+    }
+
+//    sentence encryption test
+    @Test
+    public void encryptsSentence() {
+        Encrypt encryptDecrypt = new Encrypt();
+        assertEquals("fqi", encryptDecrypt.Encrypt(2, "dog"));
+    }
 }
