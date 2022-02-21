1eb78ae0eaff377246b7ab18b656d6ace1ee2b81
priscahlimo <priscahlimoh@gmail.com>
2022-02-21 11:42:35 +0300

    add decrypt function

--- src/main/java/order/Decrypt.java
+++ src/main/java/order/Decrypt.java
@@ -1,5 +1,32 @@
 package order;
 
 public class Decrypt {
+    private int key;
+    private String reversed;
 
+    public String getReversed() {
+        return reversed;
+    }
+
+    public void setReversed(String reversed) {
+        this.reversed = reversed;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String Decrypt(int key, String reversed){
+
+        this.key= key;
+        this.reversed=reversed;
+
+        String textDecrypted="";
+
+
+    };
 }
