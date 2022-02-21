ee26a8b21ca16f3d99fac93fda7151ec69b41728
priscahlimo <priscahlimoh@gmail.com>
2022-02-21 13:05:42 +0300

    write decryption test

--- src/test/java/order/DecryptTest.groovy
+++ src/test/java/order/DecryptTest.groovy
@@ -1,9 +1,24 @@
 package order
 
 import org.junit.Test
-
+import static org.junit.Assert.*;
 class DecryptTest {
 
     @Test
+    public void checkInput_checksIfInputIsString() throws Exception {
+        Encrypt value = new Encrypt("z", 25);
+        assertEquals(true, value.isString());
+    }
 
+    @Test
+    public void checkInput_checksIfKeyIsBetween1to25() throws Exception {
+        Encrypt value = new Encrypt("z", 25);
+        assertEquals(true, value.isInRange());
+    }
+
+    @Test
+    public void decryptsCharacter(){
+        Decrypt decryptEncrypt = new Decrypt();
+        assertEquals("z",decryptEncrypt.Decrypt(1, "x"));
+    }
 }
